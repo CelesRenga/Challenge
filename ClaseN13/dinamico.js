4620e5abb674339e8c4aa38d0ddf08b79c2c7f74
Celes Web <celesbarja@gmail.com>
2023-04-30 12:34:58 -0300

    Curso Web

---
+++ ClaseN13/dinamico.js
@@ -0,0 +1,49 @@
+// var num = 3;
+// var num2 = 5;
+// var num3 = 12;
+
+// var num = prompt ('Ingrese primer valor');
+// var num2 = prompt ('Ingrese segundo valor');
+
+
+// var resultado = num + num2;
+// console.log(parseInt(num) + parseInt(num2));  //va a cambiar su variable
+// var resultado= num - num2;
+// console.log(resultado); 
+// var resultado= num / num2;
+// console.log(resultado);
+// var resultado= num * num2;
+// console.log(resultado);
+// var resultado= num % num2;
+// console.log(resultado);
+
+
+// var num = 50;
+// var num2 = 5;
+// var num3 = 22;
+
+// var loand = (num < 12) && (num3 > 20);
+// console.log(loand)
+
+// var logicaor = (num < 12) || (num3 >= 20);
+// console.log(logicaor)
+
+// console.log(!loand);
+// console.log(!logicaor);
+
+
+// const edades = 18;
+let ingresa = prompt('Ingrese su edad: ');
+
+if (ingresa < 18) {
+    console.log('Es menor de edad');
+} else if (ingresa >=18 && ingresa <=21){
+    console.log('Tiene entre 18 y 21 años');
+} else {
+    console.log('Tiene mas de 21 años');
+}
+
+
+
+
+
---
+++ ClaseN14/app.js
@@ -0,0 +1,48 @@
+// clase N° 14 - Funciones y Callback  es una funcion que se utiliza como parametro
+
+//Estructura de Function 
+///Funcion declarada  
+///function sumar(){}
+
+/// Funcion Expresada
+///const sumar = function(){};
+
+// let i = 1;
+
+// while (i <= 10) {
+// 	document.write("El valor del bucle es: " + i + "<br>");
+// 	i++;
+// }
+    
+
+// var y = 1;
+// do {
+//     document.write(y + "<br>");
+//     y = y + 1;
+// }
+
+// while(y <=9);
+
+// function saludar() {
+//     // document.write
+//     console.log('Hola mundo!');    
+// }
+// saludar();
+
+// function sumar (){
+//     let resultado = 33 + 18;
+//     return resultado;
+// }
+// let suma = sumar(); //declaro suma e inicializo con el valor que contenga sumar, que se encuentra en resultado
+// console.log(suma + 7); //58
+
+// function sumaa(x , y){
+//     return x + y;
+// }
+
+// console.log(sumaa (20,20));
+
+
+const sumarr = (a,b) => a + b;
+
+console.log(sumarr);
